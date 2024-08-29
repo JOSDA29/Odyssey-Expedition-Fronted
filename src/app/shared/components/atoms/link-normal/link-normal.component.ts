fc91a14515181efe2da1580a7b7df53b113f3a5f
jeffer <126474495+condiza@users.noreply.github.com>
2024-08-28 19:44:45 -0500

    testIntegration

--- src/app/shared/components/atoms/link-normal/link-normal.component.ts
+++ src/app/shared/components/atoms/link-normal/link-normal.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input } from '@angular/core';
+import { Router } from '@angular/router';
+import { ModalService } from '../../../../features/home/services/modal-login.service';
 
 @Component({
   selector: 'app-link-normal',
@@ -7,6 +9,17 @@ import { Component, Input } from '@angular/core';
 })
 export class LinkNormalComponent {
   @Input() text:string = '';
-  @Input() href:string = '';
+  @Input() navigateTo:string = '';
   @Input() style:'link-normal-all' | 'text-link' = 'link-normal-all';
+
+  constructor(
+    private router: Router,
+    private modalService: ModalService,
+  ) { }
+
+  navigate() {
+    this.modalService.closeModal();
+    this.router.navigate([this.navigateTo]);
+}
+
 }
